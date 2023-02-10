ca59f111ab0579a807b984321c92e310cc8dceed
raghu <raghu@LAPTOP-G1B6DQ53.mshome.net>
2023-02-10 21:11:42 +0530

    Commiting changes with Kubernetes and docker datadata

--- currency-exchange-service/src/main/java/com/bosch/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/bosch/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,9 @@ package com.bosch.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	private Environment environment;
@@ -19,6 +24,8 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} {}",from,to);
 		
 		CurrencyExchange currencyExchangeService = cuExchangeRepository.findByFromAndTo(from, to);
 		
